854033dd0de554bba3c957b73ecc820735bf6e76
Hendry Tanaka <theycantrevealus@gmail.com>
2024-12-25 11:22:23 +0700

    🔧 fix: enhance batch process

--- fact_detail.py
+++ fact_detail.py
@@ -7,7 +7,7 @@ import sys
 import subprocess
 import pandas as pd
 import urllib.parse
-
+from typing import Dict, Iterable
 from pymongo import MongoClient
 from datetime import *
 from dateutil import parser
@@ -64,7 +64,24 @@ def iterate_df(df):
     for index, row in df.iterrows():
         yield row.to_dict()
 
-def chunk_process(collection, chunksize=1, start_from=0, query={}, projection={}):
+def chunk_process(
+        collection,
+        chunk_size: int = 1,
+        start_index: int = 0,
+        query: Dict = {},
+        projection: Dict = {}
+):
+    total_docs = collection.count_documents(query)
+    chunks = [i for i in range(start_index, total_docs, chunk_size)]
+
+    for i in range(len(chunks)):
+        num_chunks = len(chunks)
+        start = chunks[i]
+        end = chunks[i] + chunk_size if i < num_chunks - 1 else total_docs
+        print("Batch : " + str(start) + " - " + str(end))
+        yield collection.find(query, projection=projection).skip(start).limit(end)
+
+def chunk_process2(collection, chunksize=1, start_from=0, query={}, projection={}):
     chunks = range(start_from, collection.count_documents(query), int(chunksize))
     num_chunks = len(chunks)
     for i in range(1,num_chunks+1):
@@ -72,6 +89,25 @@ def chunk_process(collection, chunksize=1, start_from=0, query={}, projection={}
             yield collection.find(query, projection=projection)[chunks[i-1]:chunks[i]]
         else:
             yield collection.find(query, projection=projection)[chunks[i-1]:chunks.stop]
+
+def batch_read(
+        collection,
+        query: Dict = {},
+        projection: Dict = {},
+        batch_size: int = 100
+) -> Iterable[pd.DataFrame]:
+    cursor = collection.find(query, projection=projection)
+    while True:
+        batch = []
+        for _ in range(batch_size):
+            try:
+                batch.append(next(cursor))
+            except StopIteration:
+                break
+        if not batch:
+            break
+        yield pd.DataFrame(batch)
+
 
 # =========================================================================================================================================================================
 
@@ -143,7 +179,7 @@ query ={
     }
 }
 
-output_v = {
+projection = {
     "transaction_date": 1,
     "start_date": 1,
     "end_date": 1,
@@ -176,26 +212,22 @@ output_v = {
 try:
     database = client.get_database(config['MONGO']['DATABASE'])
     collection = database.get_collection("fact_detail")
-    queryExecute = collection.find(query, output_v, batch_size=10)
-    print("======================= CURSOR ==========================")
-    print(queryExecute.explain())
-    print("")
-    print("")
-    data = list(queryExecute)
-    df = pd.DataFrame.from_records(data)
 
-    mem_usage = sys.getsizeof(df)
+    # queryExecute = collection.find(query, output_v, batch_size=10)
+    # print("======================= CURSOR ==========================")
+    # print(queryExecute.explain())
+    # print("")
+    # print("")
 
-    # CURSOR CHUNK
-    mess_chunk_iter = chunk_process(collection, BATCH_SIZE_PROCESS, 0, query, output_v)
+    # data = list(queryExecute)
+    # df = pd.DataFrame.from_records(data)
+    #
+    # mem_usage = sys.getsizeof(df)
 
-    chunk_n=0
-    total_docs=0
+
     with open(filename, "a") as txt_file:
-        for docs in mess_chunk_iter:
-            chunk_n=chunk_n+1
-            chunk_len = 0
-            for line in docs:
+        for batch in batch_read(collection, query, projection, BATCH_SIZE_PROCESS):
+            for index, line in batch.iterrows():
                 if not line["transaction_date"] == "":
                     transaction_unformatted = str(convert_datetime(str(str(line["transaction_date"]).split(".")[0].replace(' ', 'T')))).split("+")[0]
                     if not transaction_unformatted == "":
@@ -278,96 +310,6 @@ try:
                 txt_file.write(toWrite)
                 txt_file.flush()
 
-                chunk_len=chunk_len+1
-                total_docs=total_docs+1
-
-                print(f'chunk #: {chunk_n}, chunk_len: {chunk_len}')
-                print("total docs iterated: ", total_docs)
-
-    # with open(filename, "a") as txt_file:
-    #     for line in iterate_df(df):
-    #         if not line["transaction_date"] == "":
-    #             transaction_unformatted = str(convert_datetime(str(str(line["transaction_date"]).split(".")[0].replace(' ', 'T')))).split("+")[0]
-    #             if not transaction_unformatted == "":
-    #                 transaction_date = str(formatted_trx_date(transaction_unformatted)) or ""
-    #             else:
-    #                 transaction_date = ""
-    #         else:
-    #             transaction_date = ""
-    #
-    #         if not line["start_date"] == "":
-    #             start_date_unformatted = str(convert_datetime(str(str(line["start_date"]).split(".")[0].replace(' ', 'T')))).split("+")[0]
-    #             if not transaction_unformatted == "":
-    #                 start_date = str(formatted_trx_date(start_date_unformatted)) or ""
-    #             else:
-    #                 start_date = ""
-    #         else:
-    #             start_date = ""
-    #
-    #         if not line["end_date"] == "":
-    #             end_date_unformatted = str(convert_datetime(str(str(line["end_date"]).split(".")[0].replace(' ', 'T')))).split("+")[0]
-    #             if not end_date_unformatted == "":
-    #                 end_date = str(formatted_trx_date(end_date_unformatted)) or ""
-    #             else:
-    #                 end_date = ""
-    #         else:
-    #             end_date = ""
-    #
-    #         msisdn = msisdn_combine_format_to_id(line["msisdn"]) or ""
-    #         keyword = check_null(line, "keyword")
-    #         program_name = check_null(line, "program_name") or ""
-    #         program_owner = check_null(line, "program_owner") or ""
-    #         detail_program_owner = check_null(line, "detail_program_owner") or ""
-    #         created_by = check_null(line, "created_by") or ""
-    #         lifestyle = check_null(line, "liefstyle") or ""
-    #         category = check_null(line, "category") or ""
-    #         keyword_title = check_null(line, "keyword_title") or ""
-    #         SMS = check_null(line, "SMS") or ""
-    #         UMB = check_null(line, "UMB") or ""
-    #         if not line["point"] == "":
-    #             point = line["point"]
-    #         else:
-    #             point = "0"
-    #         subscriber_brand = check_null(line, "subscriber_brand") or ""
-    #         program_regional = check_null(line, "program_regional") or ""
-    #         cust_value = check_null(line, "cust_value") or ""
-    #         merchant_name = check_null(line, "merchant_name") or ""
-    #         subscriber_region = check_null(line, "subscriber_region") or ""
-    #         subscriber_branch = check_null(line, "subscriber_branch") or ""
-    #         channel_code = check_null(line, "channel_code") or ""
-    #         subsidy = check_null(line, "subsidy") or ""
-    #         subscriber_tier = check_null(line, "subscriber_tier") or ""
-    #         voucher_code = check_null(line, "voucher_code") or ""
-    #         allowed_IH = str(allowed_indihome_number(line["msisdn"])).lower()
-    #         toWrite = str(transaction_date + "|" +
-    #                       msisdn + "|" +
-    #                       keyword + "|" +
-    #                       program_name + "|" +
-    #                       program_owner + "|" +
-    #                       detail_program_owner + "|" +
-    #                       created_by + "|" +
-    #                       lifestyle + "|" +
-    #                       category + "|" +
-    #                       keyword_title + "|" +
-    #                       SMS + "|" +
-    #                       UMB + "|" +
-    #                       point + "|" +
-    #                       subscriber_brand + "|" +
-    #                       program_regional + "|" +
-    #                       cust_value + "|" +
-    #                       start_date + "|" +
-    #                       end_date + "|" +
-    #                       merchant_name + "|" +
-    #                       subscriber_region + "|" +
-    #                       subscriber_branch + "|" +
-    #                       channel_code + "|" +
-    #                       subsidy + "|" +
-    #                       subscriber_tier + "|" +
-    #                       voucher_code + "|" +
-    #                       allowed_IH + "\n")
-    #         txt_file.write(toWrite)
-    #         txt_file.flush()
-    print(f"Memory usage on cursor data frame: {mem_usage} bytes")
     client.close()
     print("===================== LINE COUNT ========================")
     subprocess.run(["wc", "-l", filename])
