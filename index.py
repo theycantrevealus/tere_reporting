9fc1ef8eccf532b437f1baa78504d6433be6dcda
Hendry Tanaka <theycantrevealus@gmail.com>
2025-01-03 22:35:02 +0700

    ✨ feat: menu for index.py

--- index.py
+++ index.py
@@ -1,69 +1,127 @@
 """ OPERATION CENTRALIZED MONITORING TOOLS """
-import pandas as pd
-from dateutil import parser
-from modules.report_fact_detail import ReportFactDetail
+import time
+import sys
+import multiprocessing
+# import curses
+# import pandas as pd
+# from dateutil import parser
+# from modules.report_fact_detail import ReportFactDetail
+from modules.logging import Logging, LoggingType
 
-i = 0
+class Unbuffered(object):
+    """ Clear buffer """
+    def __init__(self, stream):
+        self.stream = stream
 
-option_caption = [
-    'Quit',
-    'Application Manual',
-    'Database        - Back Up',
-    'Report [MANUAL] - Fact Detail',
-    'Report [MANUAL] - DCI 0POIN',
-    'Report [AUTO]   - Fact Detail',
-    'Report [AUTO]   - DCI 0POIN'
-]
+    def write(self, data):
+        """ What should I said ??? """
+        self.stream.write(data)
+        self.stream.flush()
 
-result_lists = [{'title': str(i)} for i in option_caption]
-print("*********************************************************")
-print("**  Welcome to OPERATION CENTRALIZED MONITORING TOOLS  **")
-print("*********************************************************")
-while i < len(option_caption):
-    NUMBER_LIST = str(i) + ') '
-    stitle= NUMBER_LIST + '' + result_lists[i]['title']
-    print (stitle)
-    i+=1
-print()
+    def writelines(self, datas):
+        """ What should I said ??? """
+        self.stream.writelines(datas)
+        self.stream.flush()
 
-def generate_fact_detail(parse_date: str):
-    """ Report Generate Fact Detail """
-    date_obj = pd.to_datetime(parse_date)
-    last_day = date_obj - pd.Timedelta(days=1)
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
 
-    from_date = parser.isoparse(f'{last_day.strftime("%Y-%m-%d")}T17:00:00.000Z')
-    to_date = parser.isoparse(f'{parse_date}T17:00:00.000Z')
+class Main:
+    """ Main application class """
+    def __init__(self):
+        sys.stdout = Unbuffered(sys.stdout)
 
-    fact_detail = ReportFactDetail()
-    fact_detail.produce_data(from_date,to_date)
+        # self.stdscr = curses.initscr()
+        # self.stdscr.refresh()
+        # self.stdscr.clear()
+        # curses.noecho()
+        # curses.cbreak()
 
-def get_yes_or_no(prompt):
-    """ Confirmation """
-    while True:
-        answer = input(prompt).upper()
-        if answer == 'Y':
-            return True
-        if answer == 'N':
-            return False
-        print('Invalid')
+        # Initialize all tool class
+        self.logger = Logging()
 
-while True:
-    try:
-        titlenumber = int(input('Enter number of the menu you want to choose: '))
-        if not titlenumber == 0:
-            userchoice=titlenumber
-            print('You choose: ' + str(userchoice) + ') ' + str(result_lists[userchoice]['title']))
-            if get_yes_or_no('Continue process? [y/n]: '):
-                print('Yes')
-                break
-            else:
-                print('No')
+        self.i = 0
+        self.option_caption = [
+            'Quit',
+            'Application Manual',
+            'Database        - Back Up',
+            'Report [MANUAL] - Fact Detail',
+            'Report [MANUAL] - DCI 0POIN',
+            'Report [AUTO]   - Fact Detail',
+            'Report [AUTO]   - DCI 0POIN'
+        ]
+        self.result_lists = [{'title': str(i)} for i in self.option_caption]
+        # self.show_menu()
+
+    def run(self):
+        """ RUN """
+        self.show_menu()
+        while self.i < len(self.option_caption):
+            selected= f"{str(self.i)} ) {self.result_lists[self.i]['title']}"
+            print (selected)
+            self.i += 1
+
+
+        while True:
+            try:
+                titlenumber = int(input('Enter number of the menu you want to choose: '))
+                if titlenumber != 0:
+                    userchoice=titlenumber
+                    print('You choose: ' + str(userchoice) + ') ' + str(self.result_lists[userchoice]['title']))
+                    if self.confirmation('Continue process? [y/n]: '):
+
+                        p = multiprocessing.Process(target=self.background_processor)
+                        p.daemon = True  # Set as daemon
+                        p.start()
+
+                        continue
+                    else:
+                        continue
+                else:
+                    print('Quit application')
+                    break
+
+            except ValueError:
                 continue
-        else:
-            print('Quit application')
-            break
 
-    except ValueError:
-        continue
+        print('See you later!', flush=True)
 
-print('See you later!', flush=True)
+    def show_menu(self):
+        """ Show application menu """
+        print("*********************************************************")
+        print("**  Welcome to OPERATION CENTRALIZED MONITORING TOOLS  **")
+        print("*********************************************************")
+
+
+    def confirmation(self, prompt):
+        """ Confirmation """
+        while True:
+            answer = input(prompt).upper()
+            if answer == 'Y':
+                return True
+            if answer == 'N':
+                return False
+            print('Invalid')
+
+    def background_processor(self):
+        """ Background process manager """
+        print("")
+        while True:
+            time.sleep(1)
+
+    # def cleanup(self):
+    #     """ What should I said ??? """
+    #     curses.nocbreak()
+    #     self.stdscr.keypad(False)
+    #     curses.echo()
+    #     curses.endwin()
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
+
+    # try:
+    #     main.run()
+    # finally:
+    #     main.cleanup()
--- modules/logging.py
+++ modules/logging.py
@@ -1,10 +1,9 @@
+""" LOGGING TOOLS """
 from datetime import datetime
 from enum import Enum
-# from rich import print
-# from rich.console import Console
-import shutil
 
 class LoggingType(str, Enum):
+    """ What should I said? """
     INFO = "INFO"
     SUCCESS = "SUCCESS"
     WARNING = "WARNING"
@@ -12,14 +11,17 @@ class LoggingType(str, Enum):
     GENERAL = "GENERAL"
 
 class Logging:
+    """ What should I said? """
     def __init__(self):
-        # Init logging config
         self.log_type_padding = 12
 
     def separator(self, log_type: LoggingType):
+        """ What should I said? """
         self.log(log_type, "========================================================================================================================================================================================================================================================")
 
+
     def log(self, log_type: LoggingType, wording, wording_tab = 0):
+        """ What should I said? """
         now = datetime.now()
         if log_type == 'INFO':
             print(f'[{now.strftime("%Y-%m-%d %H:%M:%S")}] - \033[94m[{log_type.center(self.log_type_padding, ' ')}]\033[0m :: {"".ljust(wording_tab, " ")} {wording}')
@@ -32,29 +34,7 @@ class Logging:
         else:
             print(f'[{now.strftime("%Y-%m-%d %H:%M:%S")}] - [{log_type.center(self.log_type_padding, ' ')}] :: {"".ljust(wording_tab, " ")} {wording}')
 
-    # def log_col(self, log_type: LoggingType, wording, column = 1, wording_tab = 0):
-        # rows, cols = shutil.get_terminal_size()
-        # print(f"Terminal size: ({rows}, {cols})")
 
-        # console = Console()
-        # print(f"Cursor position: ({console.height}, {console.width})")
-
-        # col = {
-        #     "col_1": f"\033[{rows + 2};1H",
-        #     "col_2": f"\033[{rows + 2};20H"
-        # }
-        #
-        # now = datetime.now()
-        # if log_type == 'INFO':
-        #     print(f'{col[f"col_{str(column)}"]}[{now.strftime("%Y-%m-%d %H:%M:%S")}] - \033[94m[{log_type.center(self.log_type_padding, ' ')}]\033[0m :: {"".ljust(wording_tab, " ")} {wording}')
-        # elif log_type == 'SUCCESS':
-        #     print(f'{col[f"col_{str(column)}"]}[{now.strftime("%Y-%m-%d %H:%M:%S")}] - \033[92m[{log_type.center(self.log_type_padding, ' ')}]\033[0m :: {"".ljust(wording_tab, " ")} {wording}')
-        # elif log_type == 'WARNING':
-        #     print(f'{col[f"col_{str(column)}"]}[{now.strftime("%Y-%m-%d %H:%M:%S")}] - \033[93m[{log_type.center(self.log_type_padding, ' ')}]\033[0m :: {"".ljust(wording_tab, " ")} {wording}')
-        # elif log_type == 'ERROR':
-        #     print(f'{col[f"col_{str(column)}"]}[{now.strftime("%Y-%m-%d %H:%M:%S")}] - \033[91m[{log_type.center(self.log_type_padding, ' ')}]\033[0m :: {"".ljust(wording_tab, " ")} {wording}')
-        # else:
-        #     print(f'{col[f"col_{str(column)}"]}[{now.strftime("%Y-%m-%d %H:%M:%S")}] - [{log_type.center(self.log_type_padding, ' ')}] :: {"".ljust(wording_tab, " ")} {wording}')
 
 # - Red: \033[91m
 # - Green: \033[92m
